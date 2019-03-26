df03a4dc4c253257098e24878b9406e828f13902
alulab14 <alulab14@inf.pucp.edu.pe>
2019-03-26 15:40:09 -0500

    Inicializando el bucle

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: alulab14
+ *
+ * Created on 26 de marzo de 2019, 03:32 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+#define MAX 100
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int a[MAX];
+    int b = 1;
+    for(int i=0;i<MAX;i++){
+        
+        b++;
+    }
+    return 0;
+}
+
